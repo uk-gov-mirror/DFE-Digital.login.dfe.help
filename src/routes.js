65902dfd751b9c69c492955879e672476234b2a3
Simon Bull <simon.bull.84@googlemail.com>
2018-02-26 22:37:41 +0000

    Add healthcheck

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
+const config = require('./infrastructure/config');
 const contact = require('./app/contact');
+const healthCheck = require('login.dfe.healthcheck');
 
 const mountRoutes = (app, csrf) => {
+  app.use('/healthcheck', healthCheck({ config }));
+
   app.use('/contact', contact(csrf));
 };
 
