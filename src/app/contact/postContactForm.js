6dffedda39b66084dae1483236b0df0516a08d6a
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2018-09-24 13:37:55 +0100

    send support request confirmation email

--- src/app/contact/postContactForm.js
+++ src/app/contact/postContactForm.js
@@ -62,7 +62,7 @@ const postContactForm = async (req, res) => {
   const reference = `SIR${Math.floor((new Date().getTime() - new Date(2018, 1, 1).getTime()) / 1000)}`;
 
   await notificationClient.sendSupportRequest(req.body.name, req.body.email, req.body.phone, req.body.service, req.body.type, message, reference);
-
+  await notificationClient.sendSupportRequestConfirmation(req.body.name, req.body.email, req.body.service, reference);
   req.session.reference = reference;
   return res.redirect('/contact/confirm');
 };
