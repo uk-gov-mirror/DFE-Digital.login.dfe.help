5ca3ede429448532d11dc4ee7dec7eae77f65f0e
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-03-06 11:52:03 +0000

    service access

--- src/app/contact/serviceHelp.js
+++ src/app/contact/serviceHelp.js
@@ -1,10 +1,16 @@
+const { getServiceById } = require('./../../infrastructure/applications');
+
 const get = async (req, res) => {
+  const service = await getServiceById(req.params.sid, req.id);
+  if (service && service.relyingParty && service.relyingParty.params && service.relyingParty.params.helpDestination) {
+    return res.redirect(service.relyingParty.params.helpDestination);
+  }
   const model = {
+    service,
     csrfToken: req.csrfToken(),
     validationMessages: {},
   };
   return res.render('contact/views/serviceHelp', model);
-
 };
 
 
