610b1cc1832563fc419e826898eab9d4eef3362e
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-03-15 14:33:39 +0000

    redirect route to contact

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 const config = require('./infrastructure/config');
 const contact = require('./app/contact');
 const healthCheck = require('login.dfe.healthcheck');
+const { asyncWrapper } = require('login.dfe.express-error-handling');
 
 const mountRoutes = (app, csrf) => {
   app.use('/healthcheck', healthCheck({ config }));
+
+  app.get('/', asyncWrapper((req,res) => {
+    res.redirect('/contact')
+  }));
 
   app.use('/contact', contact(csrf));
 
