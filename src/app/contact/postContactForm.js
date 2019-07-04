900b61b25be68ab30b763240be53861c055051d1
Rhys Bridges <bridgesr@uni.coventry.ac.uk>
2019-07-04 15:37:59 +0100

    NSA-2309 - remove confirmation email

--- src/app/contact/postContactForm.js
+++ src/app/contact/postContactForm.js
@@ -81,7 +81,7 @@ const postContactForm = async (req, res) => {
   const reference = `SIR${Math.floor((new Date().getTime() - new Date(2018, 1, 1).getTime()) / 1000)}`;
 
   await notificationClient.sendSupportRequest(name, email, req.body.phone, req.body.service, req.body.type, message, reference, org, req.body.urn);
-  await notificationClient.sendSupportRequestConfirmation(name, email, req.body.service, reference);
+
   req.session.reference = reference;
   return res.redirect('/contact/confirm');
 };
